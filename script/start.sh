d4f307b6d91579b1e999445a69d4966ceee8c17e
Daniel Mantovani <mantovani@daniel.oliveira.com>
2011-05-30 09:28:50 -0400

    error de pt

--- script/start.sh
+++ script/start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-perl script/truckinfo_fastcgi.pl -l /tmp/myapp.socket -n 5 -d
+SOCKET=/var/run/apps/truckinfo/app.pid
+EXEC=/home/mantovani/apps/TruckInfo/script/truckinfo_fastcgi.pl 
+if [ -f $SOCKET ] ; then
+        fuser -k $SOCKET
+fi
+
+CATALYST_DEBUG=0 perl $EXEC -l $SOCKET -n 1 -d
